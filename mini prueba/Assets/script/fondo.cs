f0730818239f15e2f8755805bd6a8aa6d68ab123
stegz <metalsteveen@gmail.com>
2018-09-13 18:00:47 -0500

    expmenu

---
+++ mini prueba/Assets/script/fondo.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fondo : MonoBehaviour
+{
+
+    //public float speed = 1.5f;
+    // Vector2 Fondopos;
+    private float x = -25.5f;
+    private float x2 = 25f;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Translate(-0.25f, 0, 0);
+       
+        if (transform.position.x < x)
+        {
+           transform.position =new Vector3(x2, 0, 0);
+            
+        }
+		
+	}
+
+    //void MoveFondo()
+    //{
+    //    Fondopos = new Vector2(Time.time * speed, 0);
+    //
+    //    GetComponent<Renderer>().material.mainTextureOffset = Fondopos;
+   // }
+}
---
+++ mini prueba/Assets/script/inicio.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class inicio : MonoBehaviour {
+
+    public Animator anim;
+    private int x = 0;
+    
+    public int vel;
+    Rigidbody2D fis;
+
+    bool ensuelo = false;
+    float radiosuelo = 0.2f;
+    public LayerMask capasuelo;
+    public Transform chesuelo;
+    public float fsalto;
+
+	// Use this for initialization
+	void Start () {
+        fis = GetComponent<Rigidbody2D> ();
+        
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown("q"))
+        {
+            x = 1;
+            anim.SetBool("correr", true);
+
+        }
+
+        if (ensuelo && Input.GetAxis("Jump") > 0)
+        {
+            fis.AddForce(Vector2.up * fsalto, ForceMode2D.Impulse);
+            anim.SetBool("suelo", false);
+            ensuelo = false;
+            
+        }
+
+        ensuelo = Physics2D.OverlapCircle(chesuelo.position, radiosuelo, capasuelo);
+        anim.SetBool("suelo", ensuelo);
+  
+    }
+
+    private void FixedUpdate()
+    {
+        if(x==1)
+        {
+            //aqui comienza a correr 
+            
+            transform.Translate(vel * Time.deltaTime, 0 ,0);
+
+        }
+ 
+    }
+
+
+   
+}
