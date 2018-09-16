be402ae28d2fb543ecd49889484ca0fd00d0eade
stegz <metalsteveen@gmail.com>
2018-09-16 11:04:42 -0500

    Boton de Audio

---
+++ proyecto v1.0/Assets/scripts/BotonAudio.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BotonAudio : MonoBehaviour {
+
+    public Image UIimagen;
+    private int x=0; // determinarea en que imagen esta 
+
+	// Use this for initialization
+	void Start ()
+    {
+        UIimagen = GameObject.Find("sonido").GetComponent<Image>();
+        UIimagen.sprite = Resources.Load<Sprite>("sprites/S1");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void click ()
+    {
+        
+        if (x == 0)
+        {        
+            x = 1;
+            UIimagen.sprite = Resources.Load<Sprite>("sprites/S2");
+        }else    
+        {
+            x = 0;
+            UIimagen.sprite = Resources.Load<Sprite>("sprites/S1");
+        }
+
+    }
+}
--- proyecto v1.0/Assets/scripts/Movopciones.cs
+++ proyecto v1.0/Assets/scripts/Movopciones.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class Movopciones : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public float speed = -2f;          // la velocidad del fondo 
+    private float posreini = -14.9f; //la posicion donde se reinicia
+    private float posini = 14.7f;      // donde aparece de nuevo 
+
+                                        
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+        transform.Translate(speed * Time.deltaTime, 0, 0);
+
+        if (transform.position.x < posreini)
+        {
+            transform.position = new Vector3(posini, 0, 0);
+
+        }
+
+
+    }
 }
