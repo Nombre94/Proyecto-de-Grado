f6dca19e262fa8f800fdade76600f4aeb06eed80
stegz <metalsteveen@gmail.com>
2018-11-06 18:39:08 -0500

    Combinacion

---
+++ Prototipo/Assets/Scripts/menus/cambioajuego.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class cambioajuego : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+
+
+    public void click()
+    {
+        SceneManager.LoadScene("Juego");
+    }
+
+}
