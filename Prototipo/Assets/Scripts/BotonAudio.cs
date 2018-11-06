2e84999eb6a4dd1ee8cceb80ca45f7a317e2bba5
Nombre94 <43253538+Nombre94@users.noreply.github.com>
2018-11-06 16:59:38 -0500

    Nuevo Prototipo
    Co-Authored-By: steveen gonzalez  <metalsteveen@gmail.com>

---
+++ Prototipo/Assets/Scripts/BotonAudio.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class BotonAudio : MonoBehaviour {
+
+   
+    private int x = 0; // determinarea en que imagen esta
+
+    // con esto agregamos los clip de audio
+    public AudioClip menu1;
+    public AudioClip menu2;
+    public AudioClip menu3;
+    public AudioClip menu4;
+
+    //donde se reproduce el sonido
+    AudioSource reproductor;
+
+    private float random;
+
+    // para el slider 
+
+    public Slider volumen;
+
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        random = 0;
+        reproductor = GetComponent<AudioSource>();
+      
+
+        random = Random.Range(1, 5);
+
+        // aqui se escoge que cancion se tocara
+
+        if (random == 1)
+        {
+            reproductor.clip = menu1;
+        }
+        if (random == 2)
+        {
+            reproductor.clip = menu2;
+        }
+        if (random == 3)
+        {
+            reproductor.clip = menu3;
+        }
+        if (random == 4)
+        {
+            reproductor.clip = menu4;
+        }
+
+        reproductor.Play();
+
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+        reproductor.volume = volumen.value;
+
+    }
+
+    public void click () // en esta clase eliminamos y ponemos el audio
+    {
+
+        if (x == 0)
+        {
+            x = 1;
+            
+            reproductor.Pause();
+        }else
+        {
+            x = 0;
+            
+            reproductor.Play();
+        }
+
+    }
+
+   
+
+//  public float musicVolume = 1f;
+//
+//	// Update is called once per frame
+//	void Update () {
+//
+//        // Setting volume option of Audio Source to be equal to musicVolume
+//        reproductor.volume = musicVolume;
+//	}
+//
+//    // Method that is called by slider game object
+//    // This method takes vol value passed by slider
+//    // and sets it as musicValue
+//    public void SetVolume(float vol)
+//    {
+//        musicVolume = vol;
+//    }
+
+}
---
+++ Prototipo/Assets/Scripts/movfondomenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movfondomenu : MonoBehaviour {
+
+    public float speed = -2f;          // la velocidad del fondo 
+    private float posreini = -24.21f; //la posicion donde se reinicia
+    private float posini = 79.9f;      // donde aparece de nuevo 
+
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Translate(speed*Time.deltaTime, 0, 0);
+
+        if (transform.position.x < posreini)
+        {
+            transform.position = new Vector3(posini, 0, 0);
+
+        }
+
+    }
+}
