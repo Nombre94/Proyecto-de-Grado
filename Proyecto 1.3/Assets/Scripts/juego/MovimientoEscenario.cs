fa28947912e573828962e8c9c854829e5dedb16c
stegz <metalsteveen@gmail.com>
2018-11-23 09:22:53 -0500

    Prefabs

---
+++ Proyecto 1.3/Assets/Scripts/juego/MovimientoEscenario.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoEscenario : MonoBehaviour {
+
+    public float vel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Translate(-vel * Time.deltaTime, 0, 0);
+
+    }
+}
