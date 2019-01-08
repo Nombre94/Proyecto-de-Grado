19755df053631488254cc08fcdcf06f9c9bf70f6
stegz <metalsteveen@gmail.com>
2019-01-08 09:59:36 -0500

    Base de la iluminacion de teclas

---
+++ Proyecto 1.3/Assets/Scripts/juego/Tecleo.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tecleo : MonoBehaviour {
+    public GameObject[] obj;
+    string name;
+    float layer;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+      if(Input.GetKeyDown("q"))
+        {
+            name = "q";
+            layer = 14;
+            generar();
+
+            
+        }
+        if (Input.GetKeyDown("w"))
+        {
+            name = "w";
+            layer = 14;
+            generar();
+
+            
+        }
+
+    }
+
+    void generar()
+    {
+        
+        if (gameObject.name == name)
+        {
+            
+            if (gameObject.layer == layer)
+            {
+                
+                Instantiate(obj[0], transform.position, Quaternion.identity);
+
+              
+            }
+           
+        }
+    }
+
+    
+}
