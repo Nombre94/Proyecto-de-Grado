0cd802b6719780918cc414d7af06f82016e89da0
stegz <metalsteveen@gmail.com>
2019-03-13 09:11:47 -0500

    arreglo diseño de teclado y lluvia de letras

--- Proyecto 1.3/Assets/Scripts/juego/sistema de palabras/wordspawn.cs
+++ Proyecto 1.3/Assets/Scripts/juego/sistema de palabras/wordspawn.cs
@@ -9,7 +9,7 @@ public class wordspawn : MonoBehaviour
 
     public worddisplay spawword()
     {
-        Vector3 randomposition = new Vector3(Random.Range(-7f, -4f), -0.5f);
+        Vector3 randomposition = new Vector3(Random.Range(-3.24f, 3.24f), -0.5f);
 
         GameObject wordobj = Instantiate(wordprefab,randomposition,Quaternion.identity, wordcanvas);
         worddisplay worddisplay = wordobj.GetComponent<worddisplay>();
